0c32fab3588f41d730572d60eb0fbe2d5202e4fb
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-06-16 19:56:00 +0200

    15_RPGInventoryUI

---
+++ Source/ActorInventoryPlugin/Public/Widgets/InventoryItemSlotTooltip.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryItemSlotTooltip.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTORINVENTORYPLUGIN_API UInventoryItemSlotTooltip : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Inventory", meta=(ExposeOnSpawn=true))
+	class UInventoryItem* Item = nullptr;
+	
+};
