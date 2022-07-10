31a4bb45c89e413314803f9e454df25f643732a2
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-07-10 17:37:49 +0200

    42_InventoryKeyAction
    Created DataAsset UInventoryKeyAction

---
+++ Source/ActorInventoryPlugin/Public/Definitions/InventoryKeyAction.h
@@ -0,0 +1,17 @@
+// All rights reserved Dominik Pavlicek 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InventoryKeyAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTORINVENTORYPLUGIN_API UInventoryKeyAction : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
