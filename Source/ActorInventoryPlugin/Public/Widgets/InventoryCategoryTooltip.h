3be30b688c2c86aa5d2794ab099af7fd771ac844
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2022-07-17 23:26:22 +0200

    50_InventoryCategoryTooltip
    New WBP C++ Class for Inventory Category Tooltip.

---
+++ Source/ActorInventoryPlugin/Public/Widgets/InventoryCategoryTooltip.h
@@ -0,0 +1,17 @@
+// All rights reserved Dominik Pavlicek 2022.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryCategoryTooltip.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTORINVENTORYPLUGIN_API UInventoryCategoryTooltip : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
