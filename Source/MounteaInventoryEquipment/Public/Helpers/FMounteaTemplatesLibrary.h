3dd014e3df295e6798121c466d9b47962582bf54
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-06-15 20:41:18 +0200

    69 Nice Templates library

---
+++ Source/MounteaInventoryEquipment/Public/Helpers/FMounteaTemplatesLibrary.h
@@ -0,0 +1,32 @@
+﻿// All rights reserved Dominik Pavlicek 2022.
+
+#pragma once
+#include "Engine/DataTable.h"
+
+template<typename Name>
+static TArray<Name> GetSourceRows(UDataTable* Table)
+{
+	if (!Table) return TArray<Name>();
+
+	return Table->GetRowNames();
+}
+
+template<class Interface, class InterfaceClass>
+static InterfaceClass GetInterfaceFrom(AActor* From)
+{
+	if (From == nullptr) return nullptr;
+
+	if (From->Implements<Interface>()) return From;
+
+	TArray<UActorComponent*> Components;
+	From->GetComponents(Components);
+
+	for (auto Itr : Components)
+	{
+		if (Itr && Itr->Implements<Interface>())
+		{
+			return Itr;
+		}
+	}
+	return nullptr;
+}
