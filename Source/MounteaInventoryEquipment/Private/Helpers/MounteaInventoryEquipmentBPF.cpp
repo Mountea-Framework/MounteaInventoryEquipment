d9b33a9d67b7d2e09198e07d1273da927d06608f
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-08-01 21:25:11 +0200

    142 Cleanup

--- Source/MounteaInventoryEquipment/Private/Helpers/MounteaInventoryEquipmentBPF.cpp
+++ Source/MounteaInventoryEquipment/Private/Helpers/MounteaInventoryEquipmentBPF.cpp
@@ -20,6 +20,40 @@ int UMounteaInventoryEquipmentBPF::CalculateMaxSubtractQuantity(UMounteaInventor
 
 int UMounteaInventoryEquipmentBPF::CalculateMaxAddQuantity(UMounteaInventoryItemBase* Item, UMounteaInventoryItemBase* OtherItem, const int32 RequestedQuantity)
 {
+	// Return 0 if RequestedQuantity is 0 or Item is null
+	if (RequestedQuantity == 0 || !Item)
+	{
+		return 0;
+	}
+    
+	// If the item is not stackable, return 0 if OtherItem is not null (updating case), or 1 if it is null (adding new item case)
+	if (!Item->ItemData.ItemQuantity.bIsStackable)
+	{
+		return OtherItem ? 0 : 1;
+	}
+    
+	// Calculate the maximum possible quantity to be added
+	const int32 CurrentQuantity = Item->ItemData.ItemQuantity.CurrentQuantity;
+	const int32 MaxQuantity = Item->ItemData.ItemQuantity.MaxQuantity;
+	int32 MaxPossible = MaxQuantity - CurrentQuantity;
+
+	// If OtherItem is not null, the requested quantity should be limited by OtherItem's current quantity
+	if (OtherItem)
+	{
+		const int32 UpdateQuantity = FMath::Min(RequestedQuantity, OtherItem->ItemData.ItemQuantity.CurrentQuantity);
+		MaxPossible = FMath::Min(MaxPossible, UpdateQuantity);
+	}
+	else // If OtherItem is null, the requested quantity should be limited by RequestedQuantity
+	{
+	MaxPossible = FMath::Min(MaxPossible, RequestedQuantity);
+	}
+
+	return MaxPossible;
+}
+
+/* TODO: Verify the new version is working
+int UMounteaInventoryEquipmentBPF::CalculateMaxAddQuantity(UMounteaInventoryItemBase* Item, UMounteaInventoryItemBase* OtherItem, const int32 RequestedQuantity)
+{
 	if (RequestedQuantity == 0)
 	{
 		return 0;
@@ -67,6 +101,7 @@ int UMounteaInventoryEquipmentBPF::CalculateMaxAddQuantity(UMounteaInventoryItem
 	
 	return 0;
 }
+*/
 
 bool UMounteaInventoryEquipmentBPF::AddItemQuantity(UMounteaInventoryItemBase* BaseItem, UMounteaInventoryItemBase* OtherItem, const int32 RequestedQuantity)
 {
