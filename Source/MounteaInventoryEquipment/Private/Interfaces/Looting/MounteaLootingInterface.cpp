74afda74814500bf1430a4224fbc79d4e0870b9c
Dominik Pavlicek <dominikpavlicek.business@gmail.com>
2023-11-19 16:33:18 +0100

    Pickup/Loot Interface
    Started implementing Pickup & Loot Interface ( #121 ).

---
+++ Source/MounteaInventoryEquipment/Private/Interfaces/Looting/MounteaLootingInterface.cpp
@@ -0,0 +1,7 @@
+﻿// All rights reserved Dominik Pavlicek 2023.
+
+
+#include "Interfaces/Looting/MounteaLootingInterface.h"
+
+
+// Add default functionality here for any IMounteaLootingInterface functions that are not pure virtual.
---
+++ Source/MounteaInventoryEquipment/Public/Interfaces/Looting/MounteaLootingInterface.h
@@ -0,0 +1,55 @@
+﻿// All rights reserved Dominik Pavlicek 2023.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Helpers/MounteaInventoryHelpers.h"
+#include "Helpers/MounteaItemHelpers.h"
+#include "UObject/Interface.h"
+#include "MounteaLootingInterface.generated.h"
+
+struct FItemSlot;
+
+class IMounteaInventoryInterface;
+
+// This class does not need to be modified.
+UINTERFACE()
+class UMounteaLootingInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MOUNTEAINVENTORYEQUIPMENT_API IMounteaLootingInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Mountea|Inventory")
+	TScriptInterface<IMounteaInventoryInterface> GetSourceInventory() const;
+	virtual TScriptInterface<IMounteaInventoryInterface> GetSourceInventory_Implementation() const = 0;
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Mountea|Inventory")
+	TScriptInterface<IMounteaInventoryInterface> GetTargetInventory() const;
+	virtual TScriptInterface<IMounteaInventoryInterface> GetTargetInventory_Implementation() const = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Mountea|Inventory")
+	TArray<FItemSlot> GetLootableItems() const;
+	virtual TArray<FItemSlot> GetLootableItems_Implementation() const = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Mountea|Inventory")
+	bool CanLootItem(const FItemSlot& Slot) const;
+	virtual bool CanLootItem_Implementation(const FItemSlot& Slot) const = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Mountea|Inventory")
+	FInventoryUpdateResult LootItem(const FItemSlot& Slot);
+	virtual FInventoryUpdateResult LootItem_Implementation(const FItemSlot& Slot) = 0;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Mountea|Inventory")
+	TArray<FInventoryUpdateResult> LootAllItems(const FItemSlot& Slots);
+	virtual TArray<FInventoryUpdateResult> LootAllItems_Implementation(const FItemSlot& Slots) = 0;
+};
