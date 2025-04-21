a4a1f923322f48bb7b64eb37858406b5f00d4baf
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-04-21 13:52:14 +0200

    Fix

--- Source/MounteaAdvancedInventorySystem/Public/Interfaces/Widgets/Category/MounteaAdvancedInventoryCategoryWidgetInterface.h
+++ Source/MounteaAdvancedInventorySystem/Public/Interfaces/Widgets/Category/MounteaAdvancedInventoryCategoryWidgetInterface.h
@@ -44,7 +44,7 @@ public:
 	 * 
 	 * @param OwningInventoryUI 
 	 */
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Inventory|UI|Items")
+	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Inventory|UI|Categories")
 	void SetOwningInventoryUI(const TScriptInterface<IMounteaAdvancedInventoryUIInterface>& OwningInventoryUI);
 	virtual void SetOwningInventoryUI_Implementation(const TScriptInterface<IMounteaAdvancedInventoryUIInterface>& OwningInventoryUI) = 0;
 };
