4d19bedd978b13df2fa21f66869de18048368a9b
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-04-23 19:37:33 +0200

    Cleanup

--- Source/MounteaAdvancedInventorySystem/Private/Interfaces/Widgets/Items/MounteaAdvancedInventoryItemSlotWidgetInterface.cpp
+++ Source/MounteaAdvancedInventorySystem/Private/Interfaces/Widgets/Items/MounteaAdvancedInventoryItemSlotWidgetInterface.cpp
@@ -1,7 +1,7 @@
 ﻿// All rights reserved Dominik Morse 2024
 
 
-#include "Interfaces/Widgets/Inventory/MounteaAdvancedInventoryItemSlotWidgetInterface.h"
+#include "Interfaces/Widgets/Items/MounteaAdvancedInventoryItemSlotWidgetInterface.h"
 
 
 // Add default functionality here for any IMounteaAdvancedInventoryItemSlotWidgetInterface functions that are not pure virtual.
--- Source/MounteaAdvancedInventorySystem/Private/Interfaces/Widgets/Items/MounteaAdvancedInventoryItemWidgetInterface.cpp
+++ Source/MounteaAdvancedInventorySystem/Private/Interfaces/Widgets/Items/MounteaAdvancedInventoryItemWidgetInterface.cpp
@@ -1,7 +1,7 @@
 ﻿// All rights reserved Dominik Morse 2024
 
 
-#include "Interfaces/Widgets/Inventory/MounteaAdvancedInventoryItemWidgetInterface.h"
+#include "Interfaces/Widgets/Items/MounteaAdvancedInventoryItemWidgetInterface.h"
 
 
 // Add default functionality here for any IMounteaAdvancedInventoryItemWidgetInterface functions that are not pure virtual.
