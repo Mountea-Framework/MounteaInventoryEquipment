3dcef57b84a396fcc58cb124608ebd8d5b341afe
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-05-01 16:25:46 +0200

    Items Grid Interface

---
+++ Source/MounteaAdvancedInventorySystem/Private/Interfaces/Widgets/Items/MounteaAdvancedInventoryItemsGridWidgetInterface.cpp
@@ -0,0 +1,7 @@
+﻿// All rights reserved Dominik Morse 2024
+
+
+#include "Interfaces/Widgets/Items/MounteaAdvancedInventoryItemsGridWidgetInterface.h"
+
+
+// Add default functionality here for any IMounteaAdvancedInventoryItemsGridWidgetInterface functions that are not pure virtual.
---
+++ Source/MounteaAdvancedInventorySystem/Public/Interfaces/Widgets/Items/MounteaAdvancedInventoryItemsGridWidgetInterface.h
@@ -0,0 +1,23 @@
+﻿// All rights reserved Dominik Morse 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "MounteaAdvancedInventoryItemsGridWidgetInterface.generated.h"
+
+UINTERFACE(MinimalAPI, BlueprintType, Blueprintable)
+class UMounteaAdvancedInventoryItemsGridWidgetInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API IMounteaAdvancedInventoryItemsGridWidgetInterface
+{
+	GENERATED_BODY()
+
+public:
+};
