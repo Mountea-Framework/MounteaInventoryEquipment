c7de8727a2c69a7c3a7f98a85413edbf01fda35a
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-05-23 16:08:22 +0200

    Interactive Widget Config

---
+++ Source/MounteaAdvancedInventorySystem/Public/Settings/TemplatesConfig/MounteaAdvancedInventoryInteractiveWidgetConfig.h
@@ -0,0 +1,25 @@
+﻿// All rights reserved Dominik Morse 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MounteaAdvancedInventoryInteractiveWidgetConfig.generated.h"
+
+/**
+ * UMounteaAdvancedInventoryInteractiveWidgetConfig is a UObject-based configuration class derived from UPrimaryDataAsset.
+ * It is designed to manage and store assets related to interactive inventory widget previews in the Mountea Advanced Inventory System.
+ */
+UCLASS()
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaAdvancedInventoryInteractiveWidgetConfig : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Preview Settings")
+	TSoftObjectPtr<UWorld> PreviewLevelAsset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Preview Settings")
+	TSoftObjectPtr<UMaterialInterface> DefaultRenderTargetMaterial;
+};
---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/Factories/MounteaAdvancedInventoryInteractiveWidgetConfig_Factory.cpp
@@ -0,0 +1,31 @@
+﻿// All rights reserved Dominik Morse 2024
+
+#include "MounteaAdvancedInventoryInteractiveWidgetConfig_Factory.h"
+#include "Utilities/MounteaAdvancedInventoryEditorUtilities.h"
+#include "Settings/TemplatesConfig/MounteaAdvancedInventoryInteractiveWidgetConfig.h"
+
+UMounteaAdvancedInventoryInteractiveWidgetConfig_Factory::UMounteaAdvancedInventoryInteractiveWidgetConfig_Factory()
+{
+	bCreateNew = true;
+	bEditAfterNew = true;
+
+	SupportedClass = UMounteaAdvancedInventoryInteractiveWidgetConfig::StaticClass();
+}
+
+UObject* UMounteaAdvancedInventoryInteractiveWidgetConfig_Factory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	return NewObject<UMounteaAdvancedInventoryInteractiveWidgetConfig>(InParent, ParentClass, Name, Flags, Context);
+}
+
+bool UMounteaAdvancedInventoryInteractiveWidgetConfig_Factory::ConfigureProperties()
+{
+	static const FText TitleText = FText::FromString(TEXT("Pick Parent Class for new Mountea Interactive Widget Config"));
+	
+	UClass* ChosenClass = nullptr;
+	const bool bPressedOk = FMounteaAdvancedInventoryEditorUtilities::PickChildrenOfClass(TitleText, ChosenClass, UMounteaAdvancedInventoryInteractiveWidgetConfig::StaticClass());
+
+	if ( bPressedOk )
+		ParentClass = ChosenClass;
+
+	return bPressedOk;
+}
