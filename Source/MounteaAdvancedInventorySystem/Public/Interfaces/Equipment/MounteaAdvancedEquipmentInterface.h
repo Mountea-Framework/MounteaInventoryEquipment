286c6ea2f1480f3e2cc5b3f08603c0e4cace455c
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-14 19:54:20 +0200

    Equipment Interface

--- Source/MounteaAdvancedInventorySystem/Public/Interfaces/Equipment/MounteaAdvancedEquipmentInterface.h
+++ Source/MounteaAdvancedInventorySystem/Public/Interfaces/Equipment/MounteaAdvancedEquipmentInterface.h
@@ -26,45 +26,5 @@ public:
 	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
 	AActor* GetOwningActor() const;
 	virtual AActor* GetOwningActor_Implementation() const = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	const FAttachmentContainer& GetAttachmentContainer() const;
-	virtual const FAttachmentContainer& GetAttachmentContainer_Implementation() const = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	bool TryAttachItem(const FInventoryItem& Item);
-	virtual bool TryAttachItem_Implementation(const FInventoryItem& Item) = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	bool AttachItem(const FInventoryItem& Item, const FName& SlotID);
-	virtual bool AttachItem_Implementation(const FInventoryItem& Item, const FName& SlotID) = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	bool TryDetachItem(const FName& SlotID);
-	virtual bool TryDetachItem_Implementation(const FName& SlotID) = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	bool DetachItem(const FName& SlotID);
-	virtual bool DetachItem_Implementation(const FName& SlotID) = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	bool IsSlotOccupied(const FName& SlotID) const;
-	virtual bool IsSlotOccupied_Implementation(const FName& SlotID) const = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	bool IsItemAttached(const FGuid& ItemGuid) const;
-	virtual bool IsItemAttached_Implementation(const FGuid& ItemGuid) const = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	FName FindFirstCompatibleSlot(const FInventoryItem& Item) const;
-	virtual FName FindFirstCompatibleSlot_Implementation(const FInventoryItem& Item) const = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	FAttachmentSlot GetSlotByID(const FName& SlotID) const;
-	virtual FAttachmentSlot GetSlotByID_Implementation(const FName& SlotID) const = 0;
-
-	UFUNCTION(BlueprintNativeEvent, Category="Mountea|Inventory & Equipment|Equipment")
-	void ClearAllEquipment();
-	virtual void ClearAllEquipment_Implementation() = 0;
 };
 
