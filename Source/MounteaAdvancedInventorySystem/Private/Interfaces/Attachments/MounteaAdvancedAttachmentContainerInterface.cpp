632f357e757a4828f433378bce88fa9e5251f3a5
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-14 19:52:31 +0200

    Attachable Container Interface

---
+++ Source/MounteaAdvancedInventorySystem/Private/Interfaces/Attachments/MounteaAdvancedAttachmentContainerInterface.cpp
@@ -0,0 +1,7 @@
+﻿// All rights reserved Dominik Morse 2024
+
+
+#include "Interfaces/Attachments/MounteaAdvancedAttachmentContainerInterface.h"
+
+
+// Add default functionality here for any IMounteaAdvancedAttachmentContainerInterface functions that are not pure virtual.
