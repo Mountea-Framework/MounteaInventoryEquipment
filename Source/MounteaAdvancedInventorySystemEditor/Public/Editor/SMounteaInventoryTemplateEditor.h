d08e5437b6c77806468e3d37084b6e51abc7576d
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-23 21:08:33 +0200

    Complete refactor

--- Source/MounteaAdvancedInventorySystemEditor/Public/Editor/SMounteaInventoryTemplateEditor.h
+++ Source/MounteaAdvancedInventorySystemEditor/Public/Editor/SMounteaInventoryTemplateEditor.h
@@ -5,155 +5,55 @@
 #include "CoreMinimal.h"
 #include "Widgets/SCompoundWidget.h"
 #include "Widgets/Views/SListView.h"
-#include "AssetThumbnail.h"
-#include "Widgets/Input/SSpinBox.h"
+#include "PropertyEditorModule.h"
+#include "IDetailsView.h"
 
-class STextPropertyEditableTextBox;
-class SClassPropertyEntryBox;
-class SGameplayTagContainerCombo;
-class SObjectPropertyEntryBox;
 class UMounteaInventoryItemTemplate;
-class SNotificationItem;
-class SMultiLineEditableTextBox;
-
 
 DECLARE_DELEGATE_OneParam(FOnTemplateChanged, UMounteaInventoryItemTemplate*);
 
-struct FAvailableStringTable
-{
-	FName TableId;
-	FText DisplayName;
-};
-
-class SMounteaInventoryTemplateEditor : public SCompoundWidget
+class SMounteaInventoryTemplateEditor : public SCompoundWidget, public FNotifyHook
 {
 public:
 	SLATE_BEGIN_ARGS(SMounteaInventoryTemplateEditor) {}
-		SLATE_EVENT(FOnTemplateChanged, OnTemplateChanged)
-	SLATE_END_ARGS()
+	SLATE_EVENT(FOnTemplateChanged, OnTemplateChanged)
+SLATE_END_ARGS()
 
-	void Construct(const FArguments& InArgs);
-	virtual ~SMounteaInventoryTemplateEditor();
+void Construct(const FArguments& InArgs);
 
 protected:
-	virtual FReply OnKeyDown(const FGeometry& MyGeometry, const FKeyEvent& InKeyEvent) override;
+	virtual void NotifyPostChange(const FPropertyChangedEvent& PropertyChangedEvent, FProperty* PropertyThatChanged) override;
 
 private:
-	void CreateNewTemplate();
+	TSharedRef<SWidget> CreatePropertyMatrix();
+	TSharedRef<SWidget> CreateToolbar();
+	TSharedRef<ITableRow> GenerateTemplateListRow(TWeakObjectPtr<UMounteaInventoryItemTemplate> Template, const TSharedRef<STableViewBase>& OwnerTable);
+	
+	TArray<UObject*> LoadAllTemplatesForMatrix();
+	void OnTemplateSelectionChanged(TWeakObjectPtr<UMounteaInventoryItemTemplate> SelectedTemplate, ESelectInfo::Type SelectInfo);
+	
+	FReply OnCreateNewTemplate();
 	FReply SaveTemplate();
+	FReply CloseTemplate();
 	FReply SaveNewTemplate();
 	FReply SaveExistingTemplate();
+
 	void ImportTemplate();
 	void ExportTemplate();
-	void RefreshTemplateList();
-	void LoadAllTemplates();
-	bool OnFilterMeshAssets(const FAssetData& AssetData) const;
-
-	TSharedRef<SWidget> CreateToolbar();
-	TSharedRef<SWidget> CreateTemplateListPanel();
-	TSharedRef<SWidget> CreateEditorPanel();
-	TSharedRef<ITableRow> GenerateTemplateListRow(TWeakObjectPtr<UMounteaInventoryItemTemplate> Template, const TSharedRef<STableViewBase>& OwnerTable);
-
-	TSharedRef<SWidget> CreateBasicInfoSection();
-	TSharedRef<SWidget> CreateItemPropertiesSection();
-	TSharedRef<SWidget> CreateWeightSystemSection();
-	TSharedRef<SWidget> CreatePriceSystemSection();
-	TSharedRef<SWidget> CreateDurabilitySystemSection();
-	TSharedRef<SWidget> CreateItemFlagsSection();
-	TSharedRef<SWidget> CreateEquipmentSection();
-	TSharedRef<SWidget> CreateVisualAssetsSection();
-	TSharedRef<SWidget> CreateMaterialsSection();
-	TSharedRef<SWidget> CreateCustomPropertiesSection();
-
-	TSharedPtr<FString> GetSelectedCategoryOption();
-	TSharedPtr<FString> GetSelectedSubCategoryOption();
-	TSharedPtr<FString> GetSelectedRarityOption();
-
-	bool CloseTemplate();
 	
 	void CreateTransientTemplate();
 	void CleanupTransientTemplate();
-	void OnTemplateSelectionChanged(TWeakObjectPtr<UMounteaInventoryItemTemplate> SelectedTemplate, ESelectInfo::Type SelectInfo);
-	void LoadTemplateData(UMounteaInventoryItemTemplate* Template);
-	void CopyTemplateData(UMounteaInventoryItemTemplate* Source, UMounteaInventoryItemTemplate* Target);
-
 	bool ValidateTemplateData(FString& ErrorMessage) const;
-
 	void ShowTemplateEditorNotification(const FString& Message, const bool bSuccess = true) const;
-	
-	void RefreshOptions();
-	TArray<TSharedPtr<FString>> GetCategoryOptions() const;
-	TArray<TSharedPtr<FString>> GetSubCategoryOptions() const;
-	TArray<TSharedPtr<FString>> GetRarityOptions() const;
 
 private:
 	FOnTemplateChanged OnTemplateChanged;
-	TSharedPtr<FAssetThumbnailPool> ThumbnailPool;
+	
+	TSharedPtr<IDetailsView> PropertyDetailsView;
 	TSharedPtr<SListView<TWeakObjectPtr<UMounteaInventoryItemTemplate>>> TemplateListView;
 	TArray<TWeakObjectPtr<UMounteaInventoryItemTemplate>> AvailableTemplates;
 	
 	TWeakObjectPtr<UMounteaInventoryItemTemplate> CurrentTemplate;
 	TWeakObjectPtr<UMounteaInventoryItemTemplate> OriginalTemplate;
 	bool bIsEditingExisting = false;
-
-	// Texts
-	TSharedPtr<STextPropertyEditableTextBox> DisplayNameWidget;
-	TSharedPtr<SEditableTextBox> DisplayNameTextBox;
-	TSharedPtr<SEditableTextBox> ItemIDTextBox;
-	TSharedPtr<SEditableTextBox> ThumbnailDescriptionTextBox;
-	TSharedPtr<SMultiLineEditableTextBox> DescriptionTextBox;
-
-	// Tags
-	TSharedPtr<SGameplayTagContainerCombo> TagsCombo;
-
-	// Selections
-	TSharedPtr<STextComboBox> CategoryCombo;
-	TSharedPtr<STextComboBox> SubCategoryCombo;
-	TSharedPtr<STextComboBox> RarityCombo;
-
-	// Price
-	TSharedPtr<SCheckBox> PriceCheckBox;
-	TSharedPtr<SSpinBox<float>> BasePriceSpinBox;
-	TSharedPtr<SSpinBox<float>> SellPriceCoefficientSpinBox;
-
-	// Quantity
-	TSharedPtr<SSpinBox<int32>> MaxQuantitySpinBox;
-	TSharedPtr<SSpinBox<int32>> MaxStackSizeSpinBox;
-
-	// Durability
-	TSharedPtr<SCheckBox> DurabilityCheckBox;
-	TSharedPtr<SSpinBox<float>> MaxDurabilitySpinBox;
-	TSharedPtr<SSpinBox<float>> BaseDurabilitySpinBox;
-	TSharedPtr<SSpinBox<float>> DurabilityPenaltySpinBox;
-	TSharedPtr<SSpinBox<float>> DurabilityToPriceSpinBox;
-
-	// Flags
-	TSharedPtr<SCheckBox> TradeableCheckBox;
-	TSharedPtr<SCheckBox> StackableCheckBox;
-	TSharedPtr<SCheckBox> CraftableCheckBox;
-	TSharedPtr<SCheckBox> DroppableCheckBox;
-	TSharedPtr<SCheckBox> ConsumableCheckBox;
-	TSharedPtr<SCheckBox> QuestItemCheckBox;
-	TSharedPtr<SCheckBox> UniqueCheckBox;
-	TSharedPtr<SCheckBox> DurableCheckBox;
-
-	// Weight
-	TSharedPtr<SCheckBox> WeightCheckBox;
-
-	// Equipment
-	TSharedPtr<SGameplayTagContainerCombo> AffectorsTagsCombo;
-	TSharedPtr<SClassPropertyEntryBox> SpawnActorPicker;
-	TSharedPtr<SObjectPropertyEntryBox> SpecialAffectPicker;
-
-	// Visuals
-	TSharedPtr<SObjectPropertyEntryBox> ItemThumbnailPicker;
-	TSharedPtr<SObjectPropertyEntryBox> ItemCoverPicker;
-	TSharedPtr<SObjectPropertyEntryBox> ItemMeshPicker;
-
-	// Cache
-	TArray<TSharedPtr<FString>> CachedCategoryOptions;
-	TArray<TSharedPtr<FString>> CachedSubCategoryOptions;
-	TArray<TSharedPtr<FString>> CachedRarityOptions;
-
-	bool bIsEditingTransient = true;
 };
