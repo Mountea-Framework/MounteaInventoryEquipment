47b187cdd9f9761530e3f0494db67b262894553c
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-28 20:36:07 +0200

    Add factory for MounteaInventoryUIComponent blueprints
    Introduces UMounteaAdvancedInventoryUIComponent_Factory to enable creation of MounteaInventoryUIComponent-based blueprints in the editor. Includes property configuration and blueprint creation logic, supporting category assignment and class selection.

---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/Factories/MounteaAdvancedInventoryUIComponent_Factory.cpp
@@ -0,0 +1,60 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MounteaAdvancedInventoryUIComponent_Factory.h"
+
+#include "Components/MounteaInventoryUIComponent.h"
+#include "Kismet2/KismetEditorUtilities.h"
+#include "Utilities/MounteaAdvancedInventoryEditorUtilities.h"
+
+UMounteaAdvancedInventoryUIComponent_Factory::UMounteaAdvancedInventoryUIComponent_Factory()
+{
+	bCreateNew = true;
+	bEditAfterNew = true;
+
+	SupportedClass = UMounteaInventoryUIComponent::StaticClass();
+}
+
+UObject* UMounteaAdvancedInventoryUIComponent_Factory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	// Something is not right!
+	if (ParentClass == nullptr || !FKismetEditorUtilities::CanCreateBlueprintOfClass(ParentClass))
+	{
+		FMessageDialog::Open(EAppMsgType::Ok, FText::Format(NSLOCTEXT("UnrealEd", "CannotCreateBlueprintFromClass", "Cannot create a blueprint based on the class '{0}'."), FText::FromString(ParentClass->GetName())));
+		return nullptr;
+	}
+
+	// Create new Blueprint
+	auto CreatedBlueprint = FKismetEditorUtilities::CreateBlueprint(
+		ParentClass,
+		InParent,
+		Name,
+		BPTYPE_Normal,
+		UBlueprint::StaticClass(),
+		UBlueprintGeneratedClass::StaticClass(),
+		NAME_None
+	);
+
+	CreatedBlueprint->HideCategories.Add("Hidden");
+	CreatedBlueprint->HideCategories.Add("Private");
+	CreatedBlueprint->HideCategories.Add("Base");
+	CreatedBlueprint->HideCategories.Add("Hide");
+	CreatedBlueprint->HideCategories.Add("Editor");
+
+	CreatedBlueprint->BlueprintCategory = FString(TEXT("Mountea Inventory & Equipment"));
+
+	return CreatedBlueprint;
+}
+
+bool UMounteaAdvancedInventoryUIComponent_Factory::ConfigureProperties()
+{
+	static const FText TitleText = FText::FromString(TEXT("Pick Parent Class for new Mountea Inventory UI Component Blueprint"));
+	ParentClass = nullptr;
+
+	UClass* ChosenClass = nullptr;
+	const bool bPressedOk = FMounteaAdvancedInventoryEditorUtilities::PickChildrenOfClass(TitleText, ChosenClass, SupportedClass);
+	if (bPressedOk)
+		ParentClass = ChosenClass;
+
+	return bPressedOk;
+}
