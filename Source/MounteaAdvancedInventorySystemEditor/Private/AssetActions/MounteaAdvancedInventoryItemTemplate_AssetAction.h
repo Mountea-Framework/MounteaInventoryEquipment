785800f3fe4bb21d07d5879412b84bdbcc04576e
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-28 20:18:39 +0200

    Add asset action for inventory item template
    Introduces FMounteaAdvancedInventoryItemTemplate_AssetAction class to define custom asset actions for UMounteaInventoryItemTemplate assets, including name, color, category, and submenu integration in the editor.

---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/AssetActions/MounteaAdvancedInventoryItemTemplate_AssetAction.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetTypeActions_Base.h"
+
+class FMounteaAdvancedInventoryItemTemplate_AssetAction : public FAssetTypeActions_Base
+{
+	FMounteaAdvancedInventoryItemTemplate_AssetAction();
+
+	virtual FText GetName() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual UClass* GetSupportedClass() const override;
+	virtual uint32 GetCategories() override;
+	virtual const TArray<FText>& GetSubMenus() const override;
+};
