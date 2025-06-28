863d6b568eff8d38d4b446daf6c61d656dfb972c
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-28 20:15:53 +0200

    Add factory for Mountea inventory item templates
    Introduces UMounteaAdvancedInventoryItemTemplate_Factory to enable creation of new MounteaInventoryItemTemplate assets in the editor. The factory supports picking a parent class and integrates with editor utilities for class selection.

---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/Factories/MounteaAdvancedInventoryItemTemplate_Factory.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MounteaAdvancedInventoryItemTemplate_Factory.h"
+
+#include "Definitions/MounteaInventoryItemTemplate.h"
+#include "Utilities/MounteaAdvancedInventoryEditorUtilities.h"
+
+
+UMounteaAdvancedInventoryItemTemplate_Factory::UMounteaAdvancedInventoryItemTemplate_Factory()
+{
+	bCreateNew = true;
+	bEditAfterNew = true;
+
+	SupportedClass = UMounteaInventoryItemTemplate::StaticClass();
+}
+
+UObject* UMounteaAdvancedInventoryItemTemplate_Factory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name,
+	EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	return NewObject<UMounteaInventoryItemTemplate>(InParent, ParentClass, Name, Flags, Context);
+}
+
+bool UMounteaAdvancedInventoryItemTemplate_Factory::ConfigureProperties()
+{
+	static const FText TitleText = FText::FromString(TEXT("Pick Parent Class for new Mountea Inventory Settings Config"));
+	
+	UClass* ChosenClass = nullptr;
+	const bool bPressedOk = FMounteaAdvancedInventoryEditorUtilities::PickChildrenOfClass
+	(
+		TitleText, ChosenClass, UMounteaInventoryItemTemplate::StaticClass()
+	);
+
+	if ( bPressedOk )
+		ParentClass = ChosenClass;
+
+	return bPressedOk;
+}
