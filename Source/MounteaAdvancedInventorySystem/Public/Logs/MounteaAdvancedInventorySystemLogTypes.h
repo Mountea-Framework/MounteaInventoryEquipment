23f69db47097cf1a4ce3f860992c2853d2f4c779
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-06-29 17:21:57 +0200

    Add copyright and license headers to source files
    Replaced placeholder copyright notices with detailed copyright and license headers in all relevant source and header files. This clarifies usage and redistribution terms for the Mountea Framework.

--- Source/MounteaAdvancedInventorySystem/Public/Logs/MounteaAdvancedInventorySystemLogTypes.h
+++ Source/MounteaAdvancedInventorySystem/Public/Logs/MounteaAdvancedInventorySystemLogTypes.h
@@ -1,9 +1,13 @@
-﻿// All rights reserved Dominik Morse 2024
-
-
-#pragma once
-
-// All rights reserved Dominik Pavlicek 2023
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
 
 #pragma once
 
