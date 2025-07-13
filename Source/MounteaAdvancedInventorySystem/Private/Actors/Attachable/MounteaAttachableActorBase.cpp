e969728073ca19c6e3d1b61c30160aed4045165c
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-07-13 11:36:48 +0200

    Add MounteaAttachableActorBase base class
    Introduces AMounteaAttachableActorBase, a new base actor class with an attachable component for the Mountea Framework. This class provides foundational attachment functionality for equipment, weapons, and other attachable objects.

---
+++ Source/MounteaAdvancedInventorySystem/Private/Actors/Attachable/MounteaAttachableActorBase.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+
+#include "Actors/Attachable/MounteaAttachableActorBase.h"
+
+#include "Components/MounteaAttachableComponent.h"
+
+AMounteaAttachableActorBase::AMounteaAttachableActorBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	AttachableComponent = CreateDefaultSubobject<UMounteaAttachableComponent>(TEXT("AttachableComponent"));
+}
+void AMounteaAttachableActorBase::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
