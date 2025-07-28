55e865cec3cccc60469c858273fb35190fd237ca
Dominik Morse <dominikpavlicek.business@gmail.com>
2025-07-28 15:19:27 +0200

    Added native Split Item Action (simple)

---
+++ Source/MounteaAdvancedInventorySystem/Public/Decorations/Templates/MounteaSplitItemStackAction.h
@@ -0,0 +1,25 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Decorations/MounteaInventorySimpleItemAction.h"
+#include "MounteaSplitItemStackAction.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=(Mountea), BlueprintType, Blueprintable, DisplayName="Split Item Stack Action")
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaSplitItemStackAction : public UMounteaInventorySimpleItemAction
+{
+	GENERATED_BODY()
+};
