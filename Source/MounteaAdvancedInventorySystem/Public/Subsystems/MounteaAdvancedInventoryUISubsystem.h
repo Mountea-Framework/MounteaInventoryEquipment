45d09d6306fd4b6d5b6dd4e67b6f8da8cdfb42c6
Dominik Morse <aenszguhelem@gmail.com>
2025-12-23 00:12:09 +0100

    Add Inventory UI subsystem and update copyright
    Introduced MounteaAdvancedInventoryUISubsystem header for UI management and updated copyright/license information in MounteaAdvancedInventoryNotificationsSubsystem.h to reflect current terms and authorship.

---
+++ Source/MounteaAdvancedInventorySystem/Public/Subsystems/MounteaAdvancedInventoryUISubsystem.h
@@ -0,0 +1,43 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/LocalPlayerSubsystem.h"
+#include "MounteaAdvancedInventoryUISubsystem.generated.h"
+
+class IMounteaAdvancedInventoryUIInterface;
+
+/**
+ * 
+ */
+UCLASS()
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaAdvancedInventoryUISubsystem : public ULocalPlayerSubsystem
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	
+	void RegisterInventoryUIManager(UObject* NewInventoryUIManager);
+	void UnregisterInventoryUIComponent(const UObject* DirtyInventoryUIManager);
+	UObject* GetInventoryUIManager() const;
+
+private:
+
+	// Reference to UI Manager, usually lives withing Player Pawn.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Managers",
+		meta=(AllowPrivateAccess))
+	TScriptInterface<IMounteaAdvancedInventoryUIInterface> InventoryUIManager;
+};
