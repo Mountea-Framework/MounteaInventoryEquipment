070578461510c792a90d73c649eb58f0b984afe5
Dominik Morse <aenszguhelem@gmail.com>
2025-12-30 15:04:06 +0100

    Import of Help resources for Help Page

--- Resources/Help/ItemTemplateEditor.js
+++ Resources/Help/ItemTemplateEditor.js
@@ -1,19 +1,17 @@
-(function() {
-    document.addEventListener('click', function(event) {
-        const link = event.target.closest('a[data-type]');
-        
-        if (!link) return;
-        
-        event.preventDefault();
-        
-        const url = link.getAttribute('href') || '';
-        const dataType = link.getAttribute('data-type') || '';
-        
-        console.log('MIAE_LINK:' + dataType + ':' + url);
-    });
-})();
-
 (function () {
+	document.addEventListener("click", function (event) {
+		const link = event.target.closest("a[data-type]");
+
+		if (!link) return;
+
+		event.preventDefault();
+
+		const url = link.getAttribute("href") || "";
+		const dataType = link.getAttribute("data-type") || "";
+
+		console.log("MIAE_LINK:" + dataType + ":" + url);
+	});
+
 	var toggles = document.querySelectorAll("[data-toggle]");
 	toggles.forEach(function (toggle) {
 		toggle.addEventListener("click", function () {
