2fbd9afa1c980c24536b521a64d0ae6f27c4b1bd
Dominik Morse <aenszguhelem@gmail.com>
2026-01-26 20:04:21 +0100

    Definition of Editor Menu Sub-Classes

---
+++ Source/MounteaAdvancedInventorySystemEditor/Public/Definitions/MounteaAdvancedInventoryEditorBaseTypes.h
@@ -0,0 +1,23 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+namespace MounteaAdvancedInventoryBaseTypes
+{
+	inline const FText TemplatesMenuEntry =				FText::FromString(TEXT("1. Templates"));
+	inline const FText InventoryBaseClassesMenuEntry =	FText::FromString(TEXT("2. Base Classes (Inventory)"));
+	inline const FText EquipmentBaseClassesMenuEntry =	FText::FromString(TEXT("3. Base Classes (Equipment)"));
+	inline const FText AssetActionsMenuEntry =			FText::FromString(TEXT("4. Item Actions"));
+	inline const FText ConfigurationMenuEntry =			FText::FromString(TEXT("666. Configuration"));
+}
