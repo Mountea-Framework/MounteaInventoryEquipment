e553ffe23ce7725d0496eeaa8e8a97daec3dd460
Dominik Morse <aenszguhelem@gmail.com>
2026-01-28 21:36:39 +0100

    Added Environmental Config Asset Action

---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/AssetActions/MounteaAdvancedInventoryInteractiveWidgetEnvConfig_AssetAction.h
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AssetTypeActions_Base.h"
+
+class  FMounteaAdvancedInventoryInteractiveWidgetEnvironmentConfig_AssetAction : public FAssetTypeActions_Base
+{
+public:
+	FMounteaAdvancedInventoryInteractiveWidgetEnvironmentConfig_AssetAction();
+
+	virtual FText GetName() const override;
+	virtual FColor GetTypeColor() const override;
+	virtual UClass* GetSupportedClass() const override;
+	virtual uint32 GetCategories() override;
+	virtual const TArray<FText>& GetSubMenus() const override;
+};
