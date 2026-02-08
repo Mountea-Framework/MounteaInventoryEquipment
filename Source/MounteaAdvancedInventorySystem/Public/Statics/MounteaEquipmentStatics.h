40c9b6783f9fc8b80015ae675f44908edc9f986d
Dominik Morse <aenszguhelem@gmail.com>
2026-02-08 14:54:07 +0100

    Introduced Equipment Statics
    Those are Equipment statics which are **not** same as Attachment statics.

---
+++ Source/MounteaAdvancedInventorySystem/Public/Statics/MounteaEquipmentStatics.h
@@ -0,0 +1,50 @@
+﻿// REPLACE COPYRIGHT
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MounteaEquipmentStatics.generated.h"
+
+class UMounteaAdvancedEquipmentSettingsConfig;
+/**
+ * 
+ */
+UCLASS()
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaEquipmentStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	
+#pragma region Settings
+
+	/**
+	 * Retrieves the advanced configuration settings for the Mountea Equipment System.
+	 * Facilitates access to equipment-specific configurations like equipment slots, allowed attachment targets etc..
+	 *
+	 * @return - A pointer to the Mountea Advanced Equipment Settings configuration object
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mountea|Inventory & Equipment|Config", 
+		meta=(CustomTag="MounteaK2Getter"),
+		DisplayName="Get Equipment Settings Config")
+	static UMounteaAdvancedEquipmentSettingsConfig* GetEquipmentSettingsConfig();
+
+	/**
+	 * Retrieves the set of scene component classes that are permitted as attachment targets
+	 * for the Mountea Equipment System. This information is defined in the advanced equipment
+	 * settings configuration and determines which components can be valid attachment targets
+	 * for equippable items.
+	 *
+	 * If no configuration is found, a default set of allowed attachment targets including
+	 * USceneComponent, UStaticMeshComponent, and USkeletalMeshComponent will be returned.
+	 *
+	 * @return - A set of soft class pointers representing allowed attachment target classes for equipment.
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mountea|Inventory & Equipment|Config", 
+		meta=(CustomTag="MounteaK2Getter"),
+		DisplayName="Get Equipment Settings Config")
+	static TSet<TSoftClassPtr<USceneComponent>> GetAllowedAttachmentTargets();
+	
+#pragma endregion
+};
