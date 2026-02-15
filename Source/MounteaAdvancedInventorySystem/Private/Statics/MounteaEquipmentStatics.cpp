42fc67360c3b7f143636f9d8554769c1b77ceb6c
Dominik Morse <aenszguhelem@gmail.com>
2026-02-15 13:12:37 +0100

    Equipment Statics update

--- Source/MounteaAdvancedInventorySystem/Private/Statics/MounteaEquipmentStatics.cpp
+++ Source/MounteaAdvancedInventorySystem/Private/Statics/MounteaEquipmentStatics.cpp
@@ -1,11 +1,30 @@
-﻿// REPLACE COPYRIGHT
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
 
 
 #include "Statics/MounteaEquipmentStatics.h"
 
+#include "Components/ActorComponent.h"
+#include "Definitions/MounteaAdvancedAttachmentSlot.h"
 #include "Definitions/MounteaInventoryItem.h"
+#include "Definitions/MounteaInventoryItemTemplate.h"
+#include "Engine/BlueprintGeneratedClass.h"
+#include "Engine/SCS_Node.h"
+#include "Engine/SimpleConstructionScript.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/Attachments/MounteaAdvancedAttachmentContainerInterface.h"
+#include "Interfaces/Equipment/MounteaAdvancedEquipmentInterface.h"
 #include "Settings/MounteaAdvancedEquipmentSettingsConfig.h"
 #include "Settings/MounteaAdvancedInventorySettings.h"
+#include "Statics/MounteaAttachmentsStatics.h"
 
 namespace EquipmentStatics
 {
@@ -34,7 +53,182 @@ TSet<TSoftClassPtr<USceneComponent>> UMounteaEquipmentStatics::GetAllowedAttachm
 	return returnValue;
 }
 
-bool UMounteaEquipmentStatics::EquipItem(const FInventoryItem& ItemDefinition, AActor*& OutSpawnedActor)
+TScriptInterface<IMounteaAdvancedEquipmentItemInterface> UMounteaEquipmentStatics::FindEquipmentItemInterface(UObject* Target)
 {
+	TScriptInterface<IMounteaAdvancedEquipmentItemInterface> result;
+
+	if (!IsValid(Target))
+		return result;
+
+	if (Target->GetClass()->ImplementsInterface(UMounteaAdvancedEquipmentItemInterface::StaticClass()))
+	{
+		result.SetObject(Target);
+		result.SetInterface(Cast<IMounteaAdvancedEquipmentItemInterface>(Target));
+		return result;
+	}
+
+	AActor* actor = Cast<AActor>(Target);
+	if (!IsValid(actor))
+		return result;
+
+	const TArray<UActorComponent*> components = actor->GetComponentsByInterface(
+		UMounteaAdvancedEquipmentItemInterface::StaticClass());
+	if (components.Num() > 0)
+	{
+		UActorComponent* component = components[0];
+		result.SetObject(component);
+		result.SetInterface(Cast<IMounteaAdvancedEquipmentItemInterface>(component));
+		return result;
+	}
+
+	const UBlueprintGeneratedClass* blueprintClass = Cast<UBlueprintGeneratedClass>(actor->GetClass());
+	if (!blueprintClass || !blueprintClass->SimpleConstructionScript)
+		return result;
+
+	for (const USCS_Node* node : blueprintClass->SimpleConstructionScript->GetAllNodes())
+	{
+		if (!node)
+			continue;
+
+		const UActorComponent* componentTemplate = node->GetActualComponentTemplate(
+			const_cast<UBlueprintGeneratedClass*>(blueprintClass));
+		if (IsValid(componentTemplate) && componentTemplate->GetClass()->ImplementsInterface(
+			UMounteaAdvancedEquipmentItemInterface::StaticClass()))
+		{
+			UActorComponent* mutableTemplate = const_cast<UActorComponent*>(componentTemplate);
+			result.SetObject(mutableTemplate);
+			result.SetInterface(Cast<IMounteaAdvancedEquipmentItemInterface>(mutableTemplate));
+			return result;
+		}
+	}
+
+	return result;
+}
+
+bool UMounteaEquipmentStatics::IsTargetClassValid(const UClass* TargetClass)
+{
+	if (!UMounteaAttachmentsStatics::IsTargetClassValid(TargetClass))
+		return false;
+	
+	if (!IsValid(TargetClass))
+		return false;
+
+	if (TargetClass->ImplementsInterface(UMounteaAdvancedEquipmentItemInterface::StaticClass()))
+		return true;
+
+	const AActor* targetActor = Cast<AActor>(TargetClass->GetDefaultObject());
+	if (!IsValid(targetActor))
+		return false;
+
+	const TArray<UActorComponent*> components = targetActor->GetComponentsByInterface(
+		UMounteaAdvancedEquipmentItemInterface::StaticClass());
+	if (components.Num() > 0)
+		return true;
+
+	const UBlueprintGeneratedClass* blueprintClass = Cast<UBlueprintGeneratedClass>(TargetClass);
+	if (!blueprintClass || !blueprintClass->SimpleConstructionScript)
+		return false;
+
+	for (const USCS_Node* node : blueprintClass->SimpleConstructionScript->GetAllNodes())
+	{
+		if (!node)
+			continue;
+
+		const UActorComponent* componentTemplate = node->GetActualComponentTemplate(
+			const_cast<UBlueprintGeneratedClass*>(blueprintClass));
+		if (IsValid(componentTemplate) && componentTemplate->GetClass()->ImplementsInterface(
+			UMounteaAdvancedEquipmentItemInterface::StaticClass()))
+			return true;
+	}
+
+	return false;
+}
+
+bool UMounteaEquipmentStatics::ValidateEquipmentItemRequest(const UObject* Outer, const FInventoryItem& ItemDefinition, UMounteaAdvancedAttachmentSlot* preferredSlot, bool& bValue)
+{
+	if (!IsValid(Outer))
+	{
+		bValue = false;
+		return true;
+	}
+
+	if (!Outer->Implements<UMounteaAdvancedAttachmentContainerInterface>() && !Outer->Implements<UMounteaAdvancedEquipmentInterface>())
+	{
+		bValue = false;
+		return true;
+	}
+
+	if (!ItemDefinition.IsItemValid())
+	{
+		bValue = false;
+		return true;
+	}
+
+	const auto spawnClass = ItemDefinition.GetTemplate()->SpawnActor.LoadSynchronous();
+	if (!IsTargetClassValid(spawnClass))
+	{
+		bValue = false;
+		return true;
+	}
+
+	const auto defaultActor = spawnClass->GetDefaultObject<AActor>();
+	if (!IsValid(defaultActor))
+	{
+		bValue = false;
+		return true;
+	}
+
+	if (!IsValid(preferredSlot))
+	{
+		const auto equipmentInterface = FindEquipmentItemInterface(defaultActor);
+		const FName preferredSlotName = equipmentInterface.GetObject() ?
+											IMounteaAdvancedEquipmentItemInterface::Execute_GetEquipmentItemPreferredSlot(equipmentInterface.GetObject()) :
+											NAME_None;
+	
+		const auto preferredSlotTag = equipmentInterface.GetObject() ?
+										  IMounteaAdvancedEquipmentItemInterface::Execute_GetEquipmentPreferredSlotTag(equipmentInterface.GetObject()) :
+										  FGameplayTag();
+	
+		if (preferredSlotName == NAME_None && !preferredSlotTag.IsValid())
+		{
+			bValue = false;
+			return true;
+		}
+
+		preferredSlot = IMounteaAdvancedAttachmentContainerInterface::Execute_IsValidSlot(Outer, preferredSlotName) ? 
+							IMounteaAdvancedAttachmentContainerInterface::Execute_GetSlot(Outer, preferredSlotName) :
+							IMounteaAdvancedAttachmentContainerInterface::Execute_GetSlot(Outer, 
+								IMounteaAdvancedAttachmentContainerInterface::Execute_FindFirstFreeSlotWithTags(Outer, FGameplayTagContainer(preferredSlotTag)));
+	}
+
+	if (!IsValid(preferredSlot))
+	{
+		bValue = false;
+		return true;
+	}
+	
+	return false;
+}
+
+bool UMounteaEquipmentStatics::EquipItem(UObject* Outer, const FInventoryItem& ItemDefinition, AActor*& OutSpawnedActor)
+{
+	OutSpawnedActor = nullptr;
+
+	UMounteaAdvancedAttachmentSlot* preferredSlot = nullptr;
+	bool bValue;
+	if (ValidateEquipmentItemRequest(Outer, ItemDefinition, preferredSlot, bValue)) 
+		return bValue;
+
+	return EquipItemToSlot(Outer, ItemDefinition, preferredSlot, OutSpawnedActor);
+}
+
+bool UMounteaEquipmentStatics::EquipItemToSlot(UObject* Outer, const FInventoryItem& ItemDefinition, UMounteaAdvancedAttachmentSlot* TargetSlot, 
+	AActor*& OutSpawnedActor)
+{
+	bool bValue;
+	
+	if (ValidateEquipmentItemRequest(Outer, ItemDefinition, TargetSlot, bValue)) 
+		return bValue;
+	
 	return true;
 }
--- Source/MounteaAdvancedInventorySystem/Public/Statics/MounteaEquipmentStatics.h
+++ Source/MounteaAdvancedInventorySystem/Public/Statics/MounteaEquipmentStatics.h
@@ -1,14 +1,24 @@
-﻿// REPLACE COPYRIGHT
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Definitions/MounteaInventoryItem.h"
+#include "Interfaces/Equipment/MounteaAdvancedEquipmentItemInterface.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "MounteaEquipmentStatics.generated.h"
 
 class UMounteaAdvancedEquipmentSettingsConfig;
-
+class UMounteaAdvancedAttachmentSlot;
 
 /**
  * 
@@ -50,15 +60,74 @@ public:
 	static TSet<TSoftClassPtr<USceneComponent>> GetAllowedAttachmentTargets();
 	
 #pragma endregion
+
+#pragma region Helpers
 	
+	/**
+	 * Finds the equipment item interface on the target object or its components.
+	 *
+	 * @param Target  Target object to inspect (typically an Actor or Component)
+	 * @return  First found equipment item interface, or empty if none found
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Mountea|Inventory & Equipment|Equipment",
+		meta=(CustomTag="MounteaK2Getter"),
+		DisplayName="Find Equipment Item Interface")
+	static TScriptInterface<IMounteaAdvancedEquipmentItemInterface> FindEquipmentItemInterface(UObject* Target);
+	
+	/**
+	 * Checks if the specified class is valid to be equipment item.
+	 * 
+	 * @param TargetClass  Target class to validate for being equipment item
+	 * @return  True if the class is valid, false otherwise
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mountea|Inventory & Equipment|Attachments|Equipment",
+		meta=(CustomTag="MounteaK2Validate"),
+		meta=(ExpandBoolAsExecs="ReturnValue"),
+		DisplayName="Is Valid Equipment Item Class")
+	static bool IsTargetClassValid(const UClass* TargetClass);
+	
+	static bool ValidateEquipmentItemRequest(const UObject* Outer, const FInventoryItem& ItemDefinition, UMounteaAdvancedAttachmentSlot* preferredSlot, 
+		bool& bValue);
+
+#pragma endregion
+
+	/**
+	 * Attempts to equip an item to the given Outer equipment based on the item's definition.
+	 * Validates the Outer object, the item definition, and determines the appropriate equipment slot.
+	 * If successful, spawns the specified actor and assigns it to the desired slot.
+	 *
+	 * @param Outer            The target object, which must implement either the
+	 *                         MounteaAdvancedAttachmentContainerInterface or MounteaAdvancedEquipmentInterface.
+	 * @param ItemDefinition   The definition of the item to be equipped. Includes data such as the item's template and spawn settings.
+	 * @param OutSpawnedActor  Output reference to the spawned actor, if the equip operation is successful.
+	 *
+	 * @return                 True if the item was successfully equipped, false otherwise.
+	 */
 	UFUNCTION(BlueprintCallable, Category="Mountea|Inventory & Equipment|Equipment",
 		meta=(CustomTag="MounteaK2Setter"),
 		DisplayName="Equip Item")
-	static bool EquipItem(const FInventoryItem& ItemDefinition, AActor*& OutSpawnedActor);
+	static bool EquipItem(UObject* Outer, const FInventoryItem& ItemDefinition, AActor*& OutSpawnedActor);
+
+	/**
+	 * Equips an inventory item to a specified attachment slot within the Mountea Equipment System.
+	 * This function spawns an actor for the provided item definition and attaches it to the specified slot.
+	 *
+	 * @param Outer              The outer context for the spawned actor, typically an owning object or actor.
+	 * @param ItemDefinition     The definition of the inventory item to be equipped.
+	 * @param TargetSlot         The target attachment slot where the item should be equipped.
+	 * @param OutSpawnedActor    Reference to the spawned actor, representing the equipped item.
+	 *                           This is set to nullptr if no actor is spawned.
+	 *
+	 * @return Returns true if the item was successfully equipped; false otherwise.
+	 */
+	UFUNCTION(BlueprintCallable, Category="Mountea|Inventory & Equipment|Equipment",
+		meta=(CustomTag="MounteaK2Setter"),
+		DisplayName="Equip Item To Slot")
+	static bool EquipItemToSlot(UObject* Outer, const FInventoryItem& ItemDefinition, UMounteaAdvancedAttachmentSlot* TargetSlot, 
+		AActor*& OutSpawnedActor);
 
 #if WITH_EDITOR
 	UFUNCTION(BlueprintInternalUseOnly)
 	bool Prototype_EquipItem(const FInventoryItem& ItemDefinition, AActor*& OutSpawnedActor) { return false; };
 #endif
-	
 };
