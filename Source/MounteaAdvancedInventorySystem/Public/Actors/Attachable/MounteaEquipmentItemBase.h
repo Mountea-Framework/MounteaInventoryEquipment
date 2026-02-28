dd57f3e506f3a0c23f1a9514a4112c3c45d63b0c
Dominik Morse <aenszguhelem@gmail.com>
2026-02-28 22:50:15 +0100

    Updated dummy attachable actor to support proxy anims

--- Source/MounteaAdvancedInventorySystem/Public/Actors/Attachable/MounteaEquipmentItemBase.h
+++ Source/MounteaAdvancedInventorySystem/Public/Actors/Attachable/MounteaEquipmentItemBase.h
@@ -13,7 +13,14 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interfaces/Equipment/MounteaAdvancedQuickUseItemInterface.h"
 #include "MounteaEquipmentItemBase.generated.h"
+
+class USceneComponent;
+class USkeletalMesh;
+class USkeletalMeshComponent;
+class UStaticMesh;
+class UStaticMeshComponent;
 
 /**
  * AMounteaEquipmentItemBase provides a base actor class with built-in equipment item functionality.
@@ -28,18 +35,32 @@ UCLASS(ClassGroup=(Mountea), Blueprintable,
 	AutoExpandCategories=("Mountea","Equipment","Mountea|Equipment"),
 	HideCategories=("Cooking","Collision"),
 	meta=(BlueprintSpawnableComponent, DisplayName="Mountea Equipment Item Base"))
-class MOUNTEAADVANCEDINVENTORYSYSTEM_API AMounteaEquipmentItemBase : public AActor
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API AMounteaEquipmentItemBase : public AActor, public IMounteaAdvancedQuickUseItemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AMounteaEquipmentItemBase();
 
+public:
+
+	virtual bool SetStaticMesh_Implementation(UStaticMesh* NewStaticMesh) override;
+	virtual bool SetSkeletalMesh_Implementation(USkeletalMesh* NewSkeletalMesh) override;
+
 protected:
 	
 	virtual void BeginPlay() override;
 	
 protected:
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Equipment")
+	TObjectPtr<USceneComponent> Root;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Equipment")
+	TObjectPtr<UStaticMeshComponent> StaticMeshComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Equipment")
+	TObjectPtr<USkeletalMeshComponent> SkeletalMeshComponent;
 	
 	/**
 	 * Equipment Item Component responsible for attaching this Actor to Player/NPC.
