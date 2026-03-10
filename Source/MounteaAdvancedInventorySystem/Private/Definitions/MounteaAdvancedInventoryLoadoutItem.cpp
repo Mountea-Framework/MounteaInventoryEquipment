83a8c2415d39d19fa3e76b454ec75f6a6fcb5887
Dominik Morse <aenszguhelem@gmail.com>
2026-03-10 18:59:05 +0100

    Add Advanced Inventory Loadout Item class
    Introduce UMounteaAdvancedInventoryLoadoutItem (header + implementation) to define configurable loadout items for the Mountea Advanced Inventory System. The UObject is BlueprintType/DefaultToInstanced and exposes properties for ItemTemplate, RandomRange, BaseQuantity, bUseRandomQuantity, bAutomaticallyEquip and EquipmentSlot (with editor GetOptions). Implements GetAvailableSlotNames() to gather allowed equipment slots from equipment settings, ApplyDefaults() to reset values, and editor logic in PostEditChangeProperty to initialize defaults, sync BaseQuantity from the item template, and automatically resolve the best equipment slot when the template provides attachment slots. Adds necessary includes and sets sensible constructor defaults.

---
+++ Source/MounteaAdvancedInventorySystem/Private/Definitions/MounteaAdvancedInventoryLoadoutItem.cpp
@@ -0,0 +1,77 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+
+#include "Definitions/MounteaAdvancedInventoryLoadoutItem.h"
+
+#include "Definitions/MounteaInventoryItemTemplate.h"
+#include "Settings/MounteaAdvancedEquipmentSettingsConfig.h"
+#include "Statics/MounteaAttachmentsStatics.h"
+#include "Statics/MounteaEquipmentStatics.h"
+
+UMounteaAdvancedInventoryLoadoutItem::UMounteaAdvancedInventoryLoadoutItem() : 
+	bUseRandomQuantity(false),
+	bAutomaticallyEquip(true)
+{
+}
+
+TArray<FName> UMounteaAdvancedInventoryLoadoutItem::GetAvailableSlotNames()
+{
+	const auto equipmentConfig = UMounteaEquipmentStatics::GetEquipmentSettingsConfig();
+	if (!IsValid(equipmentConfig))
+		return {};
+	
+	TArray<FName> returnValue;
+	returnValue.Reserve(equipmentConfig->AllowedEquipmentSlots.Num() + 1);
+	returnValue.Add(NAME_None);
+	
+	TArray<FName> definedSlots;
+	definedSlots.Reserve(equipmentConfig->AllowedEquipmentSlots.Num());
+	equipmentConfig->AllowedEquipmentSlots.GetKeys(definedSlots);
+	
+	returnValue.Append(definedSlots);
+	return returnValue;
+}
+
+void UMounteaAdvancedInventoryLoadoutItem::ApplyDefaults()
+{
+	ItemTemplate = nullptr;
+	RandomRange = FIntPoint(1, 100);
+	BaseQuantity = 1;
+	bUseRandomQuantity = false;
+	bAutomaticallyEquip = true;
+	EquipmentSlot = NAME_None;
+}
+
+#if WITH_EDITOR
+void UMounteaAdvancedInventoryLoadoutItem::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	const FName PropertyName = (PropertyChangedEvent.Property != nullptr) ? PropertyChangedEvent.Property->GetFName() : NAME_None;
+	if (PropertyName == GET_MEMBER_NAME_CHECKED(UMounteaAdvancedInventoryLoadoutItem, ItemTemplate))
+	{
+		if (!IsValid(ItemTemplate))
+		{
+			ApplyDefaults();
+		}
+		else
+		{
+			BaseQuantity = ItemTemplate->MaxQuantity;
+			if (!ItemTemplate->AttachmentSlots.IsEmpty())
+			{
+				bAutomaticallyEquip = true;				
+				EquipmentSlot = UMounteaEquipmentStatics::ResolveBestSlotIdFromTags(ItemTemplate->AttachmentSlots, ItemTemplate->EquipmentItemType);
+			}
+		}
+	}
+}
+#endif
---
+++ Source/MounteaAdvancedInventorySystem/Public/Definitions/MounteaAdvancedInventoryLoadoutItem.h
@@ -0,0 +1,72 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "MounteaAdvancedInventoryLoadoutItem.generated.h"
+
+class UMounteaInventoryItemTemplate;
+
+/**
+ * A class representing an advanced inventory loadout item in the Mountea Advanced Inventory System.
+ *
+ * This class is designed to define a single loadout item for a character or inventory system.
+ * It allows customization of the item template, quantity, and additional behaviors such as
+ * automatic equipping and slot assignment.
+ */
+UCLASS(ClassGroup=(Mountea), BlueprintType, DefaultToInstanced, EditInlineNew,
+	AutoExpandCategories=("Mountea"),
+	HideCategories=("Cooking","Collision","Private"),
+	meta=(DisplayName="Mountea Inventory Loadout Item"))
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaAdvancedInventoryLoadoutItem : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UMounteaAdvancedInventoryLoadoutItem();
+	
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration")
+	TObjectPtr<UMounteaInventoryItemTemplate> ItemTemplate = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration",
+		meta=(EditCondition="bUseRandomQuantity"))
+	FIntPoint RandomRange = FIntPoint(1, 100);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration")
+	int32 BaseQuantity = 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration")
+	uint8 bUseRandomQuantity : 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration")
+	uint8 bAutomaticallyEquip : 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration",
+		meta=(GetOptions="GetAvailableSlotNames"))
+	FName EquipmentSlot = NAME_None;
+	
+protected:
+	
+	UFUNCTION()
+	static TArray<FName> GetAvailableSlotNames();
+	
+	void ApplyDefaults();
+	
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+	
+};
