2bdf0d6abb2e1d14dc3c4d2948c533f0b3521954
Dominik Morse <aenszguhelem@gmail.com>
2026-03-10 18:59:27 +0100

    Add Mountea Advanced Inventory Loadout
    Add UMounteaAdvancedInventoryLoadout data asset (header + minimal cpp) to define predefined inventory loadouts for the Mountea inventory system. The class is a Blueprintable UPrimaryDataAsset (DisplayName: "Mountea Loadout") and exposes an Instanced TArray of TObjectPtr<UMounteaAdvancedInventoryLoadoutItem> named Items. Files include license/header comments and necessary includes.

---
+++ Source/MounteaAdvancedInventorySystem/Public/Definitions/MounteaAdvancedInventoryLoadout.h
@@ -0,0 +1,37 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MounteaAdvancedInventoryLoadout.generated.h"
+
+class UMounteaAdvancedInventoryLoadoutItem;
+
+/**
+ * Represents an advanced inventory loadout for the Mountea inventory system.
+ *
+ * This class functions as a data asset that encapsulates a collection of inventory items. It can be used
+ * to define predefined loadouts for the Mountea inventory system, enabling easier management of item sets.
+ */
+UCLASS(ClassGroup=(Mountea), Blueprintable, BlueprintType, meta=(DisplayName = "Mountea Loadout"))
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaAdvancedInventoryLoadout : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration",
+		Instanced,
+		meta=(NoResetToDefault))
+	TArray<TObjectPtr<UMounteaAdvancedInventoryLoadoutItem>> Items;
+};
