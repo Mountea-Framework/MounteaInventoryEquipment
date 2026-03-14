700fbfaa28cb7a1abd088df72b29a972a5dba40e
Dominik Morse <aenszguhelem@gmail.com>
2026-03-14 21:48:05 +0100

    Loadout Classes Factories

---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/Factories/MounteaAdvancedInventoryLoadoutConfig_Factory.cpp
@@ -0,0 +1,42 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#include "MounteaAdvancedInventoryLoadoutConfig_Factory.h"
+
+#include "Definitions/MounteaAdvancedInventoryLoadoutConfig.h"
+
+#include "Utilities/MounteaAdvancedInventoryEditorUtilities.h"
+
+UMounteaAdvancedInventoryLoadoutConfigs_Factory::UMounteaAdvancedInventoryLoadoutConfigs_Factory()
+{
+	bCreateNew = true;
+	bEditAfterNew = true;
+
+	SupportedClass = UMounteaAdvancedInventoryLoadoutConfig::StaticClass();
+}
+
+UObject* UMounteaAdvancedInventoryLoadoutConfigs_Factory::FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn)
+{
+	return NewObject<UMounteaAdvancedInventoryLoadoutConfig>(InParent, ParentClass, Name, Flags, Context);
+}
+
+bool UMounteaAdvancedInventoryLoadoutConfigs_Factory::ConfigureProperties()
+{
+	static const FText TitleText = FText::FromString(TEXT("Pick Parent Class for new Inventory & Equipment Loadouts Config"));
+	
+	UClass* ChosenClass = nullptr;
+	const bool bPressedOk = FMounteaAdvancedInventoryEditorUtilities::PickChildrenOfClass(TitleText, ChosenClass, UMounteaAdvancedInventoryLoadoutConfig::StaticClass());
+
+	if ( bPressedOk )
+		ParentClass = ChosenClass;
+
+	return bPressedOk;
+}
---
+++ Source/MounteaAdvancedInventorySystemEditor/Private/Factories/MounteaAdvancedInventoryLoadoutConfig_Factory.h
@@ -0,0 +1,35 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Factories/Factory.h"
+#include "MounteaAdvancedInventoryLoadoutConfig_Factory.generated.h"
+
+class UMounteaAdvancedInventoryLoadoutConfig;
+
+UCLASS()
+class MOUNTEAADVANCEDINVENTORYSYSTEMEDITOR_API UMounteaAdvancedInventoryLoadoutConfigs_Factory : public UFactory
+{
+	GENERATED_BODY()
+public:
+
+	UMounteaAdvancedInventoryLoadoutConfigs_Factory();
+	
+	virtual UObject* FactoryCreateNew(UClass* Class, UObject* InParent, FName Name, EObjectFlags Flags, UObject* Context, FFeedbackContext* Warn) override;
+	virtual bool ConfigureProperties() override;
+
+private:
+	// Holds the template of the class we are building
+	UPROPERTY()
+	TSubclassOf<UMounteaAdvancedInventoryLoadoutConfig> ParentClass;
+};
