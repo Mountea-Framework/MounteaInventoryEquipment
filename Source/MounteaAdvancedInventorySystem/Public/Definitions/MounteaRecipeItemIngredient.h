99d7c54c264a746e4e4d9689a5b9d79a404f11b4
Dominik Morse <aenszguhelem@gmail.com>
2026-03-15 14:16:43 +0100

    Add MounteaRecipeIngredient asset and editor logic

---
+++ Source/MounteaAdvancedInventorySystem/Public/Definitions/MounteaRecipeItemIngredient.h
@@ -0,0 +1,40 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MounteaRecipeItemIngredient.generated.h"
+
+class UMounteaInventoryItemTemplate;
+
+/**
+ * 
+ */
+UCLASS(ClassGroup=(Mountea), BlueprintType, DefaultToInstanced, EditInlineNew,
+	AutoExpandCategories=("Mountea"),
+	HideCategories=("Cooking","Collision","Private"),
+	meta=(DisplayName="Mountea Inventory Recipe Item Ingredient"))
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaRecipeItemIngredient : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration",
+		meta=(NoResetToDefault))
+	TObjectPtr<UMounteaInventoryItemTemplate> IngredientSource = nullptr;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Configuration",
+		meta=(NoResetToDefault))
+	int32 RequiredQuantity = 1;
+};
