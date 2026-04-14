dfcb111c1118e87a95fd60403350d0eb146fc3f6
Dominik Morse <aenszguhelem@gmail.com>
2026-04-14 21:12:55 +0200

    Add: Crafting Participant Component

---
+++ Source/MounteaAdvancedInventorySystem/Private/Components/MounteaCraftingParticipantComponent.cpp
@@ -0,0 +1,133 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+
+#include "Components/MounteaCraftingParticipantComponent.h"
+
+#include "Definitions/MounteaCraftingBaseDataTypes.h"
+#include "Definitions/MounteaRecipeTemplate.h"
+#include "Statics/MounteaCraftingStatics.h"
+
+UMounteaCraftingParticipantComponent::UMounteaCraftingParticipantComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UMounteaCraftingParticipantComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+}
+
+TSet<UMounteaRecipeTemplate*> UMounteaCraftingParticipantComponent::GetKnownRecipes_Implementation() const
+{
+	return ResolveKnownRecipeTemplates(KnownRecipes);
+}
+
+TArray<UMounteaRecipeTemplate*> UMounteaCraftingParticipantComponent::GetRecipes_Implementation(const FGameplayTag& CraftingStationType) const
+{
+	const TSet<UMounteaRecipeTemplate*> knownRecipes = ResolveKnownRecipeTemplates(KnownRecipes);
+	if (knownRecipes.Num() == 0)
+		return TArray<UMounteaRecipeTemplate*>();
+
+	TArray<UMounteaRecipeTemplate*> result;
+	result.Reserve(knownRecipes.Num());
+
+	for (UMounteaRecipeTemplate* recipe : knownRecipes)
+	{
+		if (!IsValid(recipe))
+		continue;
+
+		if (!CraftingStationType.IsValid() || recipe->RequiredCraftingPlace == CraftingStationType)
+			result.Add(recipe);
+	}
+
+	return result;
+}
+
+UMounteaRecipeTemplate* UMounteaCraftingParticipantComponent::GetRecipe_Implementation(const FGuid& RecipeGuid) const
+{
+	if (!KnownRecipes.Contains(RecipeGuid))
+		return nullptr;
+
+	const TSet<UMounteaRecipeTemplate*> knownRecipes = ResolveKnownRecipeTemplates(KnownRecipes);
+	UMounteaRecipeTemplate* const* foundRecipe = knownRecipes.Array().FindByPredicate(
+		[&RecipeGuid](const UMounteaRecipeTemplate* recipe)
+		{
+			return IsValid(recipe) && recipe->RecipeGuid == RecipeGuid;
+		});
+
+	return foundRecipe != nullptr ? *foundRecipe : nullptr;
+}
+
+bool UMounteaCraftingParticipantComponent::IsRecipeKnown_Implementation(UMounteaRecipeTemplate* RecipeTemplate) const
+{
+	return KnownRecipes.Contains(RecipeTemplate->RecipeGuid);
+}
+
+bool UMounteaCraftingParticipantComponent::LearnRecipe_Implementation(UMounteaRecipeTemplate* RecipeTemplate)
+{
+	if (Execute_IsRecipeKnown(this, RecipeTemplate))
+		return false;
+	KnownRecipes.Add(RecipeTemplate->RecipeGuid);
+	
+	OnRecipeLearned.Broadcast(RecipeTemplate);
+	return true;
+}
+
+bool UMounteaCraftingParticipantComponent::ForgetRecipe_Implementation(UMounteaRecipeTemplate* RecipeTemplate)
+{
+	if (!Execute_IsRecipeKnown(this, RecipeTemplate))
+		return false;
+	OnRecipeForgotten.Broadcast(RecipeTemplate);
+	
+	KnownRecipes.Remove(RecipeTemplate->RecipeGuid);
+	return true;
+}
+
+bool UMounteaCraftingParticipantComponent::IsCraftingPossible_Implementation(UMounteaRecipeTemplate* TemplateToCraft) const
+{
+	return Execute_IsRecipeKnown(this, TemplateToCraft);
+}
+
+FMounteaCraftingResult UMounteaCraftingParticipantComponent::StartCrafting_Implementation(UMounteaRecipeTemplate* TemplateToCraft, UMounteaRecipeIngredientsList* Ingredients)
+{
+	FMounteaCraftingResult result = UMounteaCraftingStatics::CraftItem(this, TemplateToCraft, Ingredients);
+	return result;
+}
+
+TSet<UMounteaRecipeTemplate*> UMounteaCraftingParticipantComponent::ResolveKnownRecipeTemplates(const TSet<FGuid>& KnownRecipeGuids)
+{
+	if (KnownRecipeGuids.Num() == 0)
+		return TSet<UMounteaRecipeTemplate*>();
+
+	const TSet<UMounteaRecipeTemplate*> allRecipes = UMounteaCraftingStatics::GetAllRecipeTemplates();
+	if (allRecipes.Num() == 0)
+		return TSet<UMounteaRecipeTemplate*>();
+
+	TSet<UMounteaRecipeTemplate*> result;
+	result.Reserve(KnownRecipeGuids.Num());
+
+	const auto allRecipesArray = allRecipes.Array();
+	for (const FGuid& knownRecipeGuid : KnownRecipeGuids)
+	{
+		UMounteaRecipeTemplate* const* foundRecipe = allRecipesArray.FindByPredicate(
+			[&knownRecipeGuid](const UMounteaRecipeTemplate* recipe)
+			{
+				return IsValid(recipe) && recipe->RecipeGuid == knownRecipeGuid;
+			});
+
+		if (foundRecipe != nullptr && IsValid(*foundRecipe))
+			result.Add(*foundRecipe);
+	}
+
+	return result;
+}
+
---
+++ Source/MounteaAdvancedInventorySystem/Public/Components/MounteaCraftingParticipantComponent.h
@@ -0,0 +1,76 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not 
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Interfaces/Crafting/MounteaAdvancedCraftingParticipantInterface.h"
+#include "MounteaCraftingParticipantComponent.generated.h"
+
+struct FMounteaCraftingResult;
+
+UCLASS(ClassGroup=(Mountea), Blueprintable,
+	AutoExpandCategories=("Mountea","Crafting","Mountea|Crafting"),
+	HideCategories=("Cooking","Collision"),
+	meta=(BlueprintSpawnableComponent, DisplayName="Mountea Crafting Participant Component"))
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaCraftingParticipantComponent : public UActorComponent, public IMounteaAdvancedCraftingParticipantInterface
+{
+	GENERATED_BODY()
+
+public:
+	UMounteaCraftingParticipantComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	
+	virtual TSet<UMounteaRecipeTemplate*> GetKnownRecipes_Implementation() const override;
+	virtual TArray<UMounteaRecipeTemplate*> GetRecipes_Implementation(const FGameplayTag& CraftingStationType) const override;
+	virtual UMounteaRecipeTemplate* GetRecipe_Implementation(const FGuid& RecipeGuid) const override;
+	virtual bool IsRecipeKnown_Implementation(UMounteaRecipeTemplate* RecipeTemplate) const override;
+	virtual bool LearnRecipe_Implementation(UMounteaRecipeTemplate* RecipeTemplate) override;
+	virtual bool ForgetRecipe_Implementation(UMounteaRecipeTemplate* RecipeTemplate) override;
+	virtual bool IsCraftingPossible_Implementation(UMounteaRecipeTemplate* TemplateToCraft) const override;
+	virtual FMounteaCraftingResult StartCrafting_Implementation(UMounteaRecipeTemplate* TemplateToCraft, UMounteaRecipeIngredientsList* Ingredients) override;
+	
+	virtual FOnCraftingFinished& GetOnCraftingFinishedEventHandle() override
+	{
+		return OnCraftingFinished;
+	}
+	virtual FOnRecipeLearned& GetOnRecipeLearnedEventHandle() override
+	{
+		return OnRecipeLearned;
+	}
+	virtual FOnRecipeForgotten& GetOnRecipeForgottenEventHandle() override
+	{
+		return OnRecipeForgotten;
+	}
+	
+protected:
+	static TSet<UMounteaRecipeTemplate*> ResolveKnownRecipeTemplates(const TSet<FGuid>& KnownRecipeGuids);
+	
+public:
+	
+	UPROPERTY(BlueprintAssignable, Category="Crafting")
+	FOnCraftingFinished OnCraftingFinished;
+	
+	UPROPERTY(BlueprintAssignable, Category="Crafting")
+	FOnRecipeLearned OnRecipeLearned;
+	
+	UPROPERTY(BlueprintAssignable, Category="Crafting")
+	FOnRecipeForgotten OnRecipeForgotten;
+	
+	UPROPERTY(SaveGame, VisibleDefaultsOnly, BlueprintReadOnly, Category="Crafting",
+		meta=(NoResetToDefault))
+	TSet<FGuid> KnownRecipes;
+};
