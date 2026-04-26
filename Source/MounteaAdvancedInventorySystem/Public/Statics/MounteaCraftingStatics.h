36edb672d179263b8e6a8cc0a3fc5d5dc8a296fd
Dominik Morse <aenszguhelem@gmail.com>
2026-04-26 14:32:32 +0200

    Add: Crafting UI Statics

--- Source/MounteaAdvancedInventorySystem/Public/Statics/MounteaCraftingStatics.h
+++ Source/MounteaAdvancedInventorySystem/Public/Statics/MounteaCraftingStatics.h
@@ -165,4 +165,16 @@ public:
 		meta=(CustomTag="MounteaK2Setter"),
 		DisplayName="Stop Using Station")
 	static bool StopUsing(UObject* Target, const TScriptInterface<IMounteaAdvancedCraftingParticipantInterface>& Participant);
+	
+/**
+ * HELPERS
+ */
+
+public:
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mountea|Inventory & Equipment|Crafting|Helpers",
+		meta=(CustomTag="MounteaK2Getter"),
+		meta=(AutoCreateRefTerm="CategoryTag"),
+		DisplayName="Get Recipes By Category")
+	static TArray<UMounteaRecipeTemplate*> GetRecipesByCategory(UObject* Target, UPARAM(meta=(Categories="Mountea_Inventory.Categories,Mountea_Inventory.Category,Categories,Category")) const FGameplayTag& CategoryTag);
 };
---
+++ Source/MounteaAdvancedInventorySystem/Public/Statics/MounteaCraftingUIStatics.h
@@ -0,0 +1,34 @@
+﻿// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MounteaCraftingUIStatics.generated.h"
+
+class UMounteaAdvancedCraftingUIConfig;
+
+/**
+ * 
+ */
+UCLASS()
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaCraftingUIStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Mountea|Inventory & Equipment|Config", 
+		meta=(CustomTag="MounteaK2Getter"),
+		DisplayName="Get Crafting UI Config")
+	static UMounteaAdvancedCraftingUIConfig* GetCraftingUISettingsConfig();
+};
