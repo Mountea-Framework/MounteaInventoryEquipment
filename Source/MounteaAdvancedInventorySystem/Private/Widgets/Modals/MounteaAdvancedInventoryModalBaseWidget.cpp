0111932e4bd932c03a9fdf7acae228dc7c2a24b0
Dominik Morse <aenszguhelem@gmail.com>
2026-06-02 15:49:04 +0200

    A: Modal Base Widget Classes

---
+++ Source/MounteaAdvancedInventorySystem/Private/Widgets/Modals/MounteaAdvancedInventoryModalBaseWidget.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright (C) 2026 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#include "Widgets/Modals/MounteaAdvancedInventoryModalBaseWidget.h"
+
+#include "Components/PanelWidget.h"
+#include "Interfaces/Widgets/Modal/MounteaAdvancedInventoryModalContentWidgetInterface.h"
+
+void UMounteaAdvancedInventoryModalBaseWidget::AddModalContentToModalWindow_Implementation(UUserWidget* ModalContentWidget, UMounteaModalsPayload* Payload)
+{
+	if (!IsValid(ModalContentWidget))
+		return;
+
+	if (IMounteaAdvancedInventoryModalContentWidgetInterface* previousContent = Cast<IMounteaAdvancedInventoryModalContentWidgetInterface>(CurrentModalContentWidget))
+	{
+		previousContent->GetOnModalContentConfirmedHandle().RemoveDynamic(this, &UMounteaAdvancedInventoryModalBaseWidget::HandleModalContentConfirmed);
+		previousContent->GetOnModalContentCancelledHandle().RemoveDynamic(this, &UMounteaAdvancedInventoryModalBaseWidget::HandleModalContentCancelled);
+	}
+
+	CurrentModalContentWidget = ModalContentWidget;
+
+	if (IMounteaAdvancedInventoryModalContentWidgetInterface* newContent = Cast<IMounteaAdvancedInventoryModalContentWidgetInterface>(CurrentModalContentWidget))
+	{
+		newContent->GetOnModalContentConfirmedHandle().AddUniqueDynamic(this, &UMounteaAdvancedInventoryModalBaseWidget::HandleModalContentConfirmed);
+		newContent->GetOnModalContentCancelledHandle().AddUniqueDynamic(this, &UMounteaAdvancedInventoryModalBaseWidget::HandleModalContentCancelled);
+	}
+
+	OnModalContentAddedToModalWindow.Broadcast(ModalContentWidget, Payload);
+}
+
+void UMounteaAdvancedInventoryModalBaseWidget::HandleModalContentConfirmed(UObject* Payload)
+{
+	IMounteaAdvancedInventoryModalWidgetInterface::Execute_ModalContentConfirmed(this, Payload);
+	OnModalConfirmed.Broadcast(Payload);
+}
+
+void UMounteaAdvancedInventoryModalBaseWidget::HandleModalContentCancelled()
+{
+	IMounteaAdvancedInventoryModalWidgetInterface::Execute_ModalContentCancelled(this);
+	OnModalCancelled.Broadcast();
+}
---
+++ Source/MounteaAdvancedInventorySystem/Public/Widgets/Modals/MounteaAdvancedInventoryModalBaseWidget.h
@@ -0,0 +1,66 @@
+﻿// Copyright (C) 2026 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Blueprint/UserWidget.h"
+#include "Interfaces/Widgets/MounteaInventoryGenericWidgetInterface.h"
+#include "Interfaces/Widgets/Modal/MounteaAdvancedInventoryModalWidgetInterface.h"
+#include "MounteaAdvancedInventoryModalBaseWidget.generated.h"
+
+class UPanelWidget;
+class UMounteaModalsPayload;
+
+/**
+ * Base modal window widget capable of hosting modal content widgets.
+ */
+UCLASS(Blueprintable, BlueprintType)
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaAdvancedInventoryModalBaseWidget : public UUserWidget,
+	public IMounteaAdvancedInventoryModalWidgetInterface, public IMounteaInventoryGenericWidgetInterface
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void AddModalContentToModalWindow_Implementation(UUserWidget* ModalContentWidget, UMounteaModalsPayload* Payload) override;
+	virtual FOnModalContentAddedToModalWindow& GetOnModalContentAddedToModalWindowHandle() override
+	{
+		return OnModalContentAddedToModalWindow;
+	};
+
+	virtual void ProcessInventoryWidgetCommand_Implementation(const FString& Command, UObject* OptionalPayload = nullptr) override {};
+	virtual void ApplyTheme_Implementation() override {};
+	virtual void RefreshWidget_Implementation() override {};
+	virtual void ConsumeUIInput_Implementation(const FGameplayTag& InputTag, const FMounteaWidgetInputPayload& Payload, float DeltaTime) override {};
+
+	virtual FGameplayTag GetWidgetTag_Implementation() const override
+	{
+		return WidgetTag;
+	};
+	virtual void SetWidgetTag_Implementation(const FGameplayTag& NewWidgetTag) override
+	{
+		if (WidgetTag != NewWidgetTag)
+			WidgetTag = NewWidgetTag;
+	};
+
+	UPROPERTY(BlueprintReadOnly, Category="Widgets|Modal")
+	TObjectPtr<UUserWidget> CurrentModalContentWidget = nullptr;
+
+	UPROPERTY(BlueprintAssignable, Category="Events|Modal")
+	FOnModalContentAddedToModalWindow OnModalContentAddedToModalWindow;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Primary Data",
+		meta=(NoResetToDefault),
+		meta=(Categories="Mountea_Inventory.WidgetState.Modal,State"))
+	FGameplayTag WidgetTag;
+};
