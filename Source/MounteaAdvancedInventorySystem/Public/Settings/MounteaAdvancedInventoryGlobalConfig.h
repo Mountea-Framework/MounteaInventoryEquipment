005d6ceccd60e50dfde5659699cd5f9da9a3d470
Dominik Morse <aenszguhelem@gmail.com>
2026-06-14 12:51:23 +0200

    A: Global Config

---
+++ Source/MounteaAdvancedInventorySystem/Public/Settings/MounteaAdvancedInventoryGlobalConfig.h
@@ -0,0 +1,99 @@
+// Copyright (C) 2025 Dominik (Pavlicek) Morse. All rights reserved.
+//
+// Developed for the Mountea Framework as a free tool. This solution is provided
+// for use and sharing without charge. Redistribution is allowed under the following conditions:
+//
+// - You may use this solution in commercial products, provided the product is not
+//   this solution itself (or unless significant modifications have been made to the solution).
+// - You may not resell or redistribute the original, unmodified solution.
+//
+// For more information, visit: https://mountea.tools
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "MounteaAdvancedInventoryGlobalConfig.generated.h"
+
+UENUM(BlueprintType)
+enum class EMounteaJsonDefinitionFieldType : uint8
+{
+	Int			UMETA(DisplayName="Int"),
+	Int64		UMETA(DisplayName="Int64"),
+	Float		UMETA(DisplayName="Float"),
+	Bool		UMETA(DisplayName="Bool"),
+	String		UMETA(DisplayName="String"),
+	Name		UMETA(DisplayName="Name"),
+	Text		UMETA(DisplayName="Text"),
+	Byte		UMETA(DisplayName="Byte"),
+	Object		UMETA(DisplayName="Object"),
+	SoftObject	UMETA(DisplayName="Soft Object"),
+	Class		UMETA(DisplayName="Class"),
+	SoftClass	UMETA(DisplayName="Soft Class"),
+	JsonObject	UMETA(DisplayName="Json Object"),
+	Struct		UMETA(DisplayName="Struct")
+};
+
+USTRUCT(BlueprintType)
+struct MOUNTEAADVANCEDINVENTORYSYSTEM_API FMounteaJsonObjectDefinitionField
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="JSON Definition",
+		meta=(NoResetToDefault))
+	FName FieldName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="JSON Definition",
+		meta=(NoResetToDefault))
+	EMounteaJsonDefinitionFieldType FieldType = EMounteaJsonDefinitionFieldType::String;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="JSON Definition",
+		meta=(NoResetToDefault))
+	bool bRequired = true;
+
+public:
+
+	bool IsValid() const
+	{
+		return !FieldName.IsNone();
+	}
+};
+
+USTRUCT(BlueprintType)
+struct MOUNTEAADVANCEDINVENTORYSYSTEM_API FMounteaJsonObjectDefinition
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="JSON Definition",
+		meta=(NoResetToDefault),
+		meta=(TitleProperty="{FieldName} | {FieldType}"))
+	TArray<FMounteaJsonObjectDefinitionField> Fields;
+
+public:
+
+	bool IsValid() const
+	{
+		return Fields.Num() > 0;
+	}
+};
+
+UCLASS(ClassGroup=(Mountea), BlueprintType, Blueprintable, DisplayName="Global Config",
+	meta=(ShortTooltip="Global configuration asset for Mountea Inventory & Equipment shared definitions."))
+class MOUNTEAADVANCEDINVENTORYSYSTEM_API UMounteaAdvancedInventoryGlobalConfig : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+
+	UMounteaAdvancedInventoryGlobalConfig();
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="JSON Definitions",
+		meta=(ForceInlineRow, NoResetToDefault))
+	TMap<FString, FMounteaJsonObjectDefinition> JsonObjectDefinitions;
+};
